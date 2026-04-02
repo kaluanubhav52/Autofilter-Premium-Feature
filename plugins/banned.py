e3332a6d156dbcacbf2d71bb71ba77b4b42582b9
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-02 10:01:27 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,8 +1,8 @@
-from pyrogram import Client, filters
+from kurigram import Client, filters
 from utils import temp
-from pyrogram.types import Message
+from kurigram.types import Message
 from database.users_chats_db import db
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from kurigram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import SUPPORT_CHAT
 
 async def banned_users(_, client, message: Message):
